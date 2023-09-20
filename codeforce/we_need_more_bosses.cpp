28a09ca9cdab3c20d682b693c014ed984e248391
Ignacio <ignaciooromendia@gmail.com>
2023-09-20 00:10:37 -0300

    DFS with timer

--- codeforce/we_need_more_bosses.cpp
+++ codeforce/we_need_more_bosses.cpp
@@ -5,79 +5,58 @@ using namespace std;
 
 // judge: https://codeforces.com/contest/1000/problem/E
 
-// Constants
-const int NOT_VISITED = -1;
-const int PROCESSING = 0;
-const int VISITED = 1;
-
 // Variables
+int n, timer;
 vector<vector<int> > adj;
-vector<vector<int> > treeEdges;
-vector<int> backedge_lower_limite_on;
-vector<int> backedge_upper_limite_on;
-vector<int> state;
-vector<int> memo;
-int n, m;
+vector<int> time_in, low;
+vector<bool> visited;
+int num_bridges = 0;
 
-void dfs(int v, int parent = -1) {
-    state[v] = PROCESSING;
+/// @param time_in = the time when we first visit v
+/// @param low     = the lowest tin of any node that is reachable from v
+/// for the node v,  low[v] = min(tin[v], tin[p] for all p that (v,p) is a back edge, low[u] for all (v,u) is a tree edge)
+
+// **** The edge (v,u) is a bridge if and only if low[u] > tin[v] ****
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    time_in[v] = low[v] = timer++;
+
     for(int u: adj[v]) {
-        if (state[u] == NOT_VISITED) {
-            // Generating a tree
-            treeEdges[v].push_back(u);
+        if (u == p) continue;
+        if (visited[u]) {
+            // (v,u) is a back edge
+            low[v] = min(low[v], time_in[u]);
+        } else {
+            // (v,u) is a tree edge
             dfs(u, v);
-        } else if (u != parent and state[u] == PROCESSING) {
-            backedge_lower_limite_on[v]++;
-            backedge_upper_limite_on[u]++;
+            low[v] = min(low[v], low[u]);
+            if (low[u] > time_in[v]) {
+                // (v,u) is a bridge
+                num_bridges++;
+            }
         }
     }
-
-    state[v] = VISITED;
 }
 
-int covered(int v, int parent = -1) {
-    if (memo[v] != -1) return memo[v];
-    int res = 0;
-    for (int son : treeEdges[v]) {
-        if (son != parent) {
-            res += covered(son, v);
-        }
-    }
-    res -= backedge_upper_limite_on[v];
-    res += backedge_lower_limite_on[v];
-
-    return memo[v] = res;
-}
-
-int maximum_boses() {
-    int components = 0;
-    for(int i = 0; i <= n; i++) {
-        if (state[i] == NOT_VISITED) {
+void find_bridges() {
+    timer = 0;
+    for(int i = 0; i < n+1; i++) {
+        if(!visited[i]) {
             dfs(i);
-            components++;
         }
     }
-
-    int bridges = 0;
-    for(int i = 0; i <= n; i++) {
-        if (covered(i) == 0) bridges++;
-    }
-
-    return bridges - components;
 }
-
 
 int main() {
+    int m;
     cin >> n >> m;
 
-    adj       = vector<vector<int> >(n+1);
-    treeEdges = vector<vector<int> >(n+1);
-
-    backedge_lower_limite_on = vector<int>(n+1, 0);
-    backedge_upper_limite_on = vector<int>(n+1, 0);
-
-    state = vector<int>(n+1, NOT_VISITED);
-    memo  = vector<int>(n+1, -1);
+    adj     = vector<vector<int> >(n+1);
+    time_in = vector<int>(n+1, -1);
+    low     = vector<int>(n+1, -1);
+    visited = vector<bool>(n+1, false);
+    
 
     while(m--) {
         int u, v;
@@ -86,5 +65,8 @@ int main() {
         adj[v].push_back(u);
     }
 
-    cout << maximum_boses() << endl;
+    find_bridges();
+
+    cout << num_bridges << endl;
+
 }
