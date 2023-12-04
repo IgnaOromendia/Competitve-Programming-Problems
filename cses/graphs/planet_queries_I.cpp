552bef3f3c062ae37027de2bfe008ff29d26adb1
Ignacio <ignaciooromendia@gmail.com>
2023-12-03 21:28:19 -0300

    Optimization

--- cses/graphs/planet_queries_I.cpp
+++ cses/graphs/planet_queries_I.cpp
@@ -2,15 +2,15 @@
 #include<vector>
 #include<math.h>
 #include<algorithm>
-
+ 
 #define INF 1e8;
 #define NOT_VISITED -1
 #define VISITING 0
 #define VISITED 1
-
+ 
 using namespace std;
 typedef pair<int, int> query;
-
+ 
 int n, q, cycle_tag = 0;
 vector<query> queries;
 vector<int> adj, state;
@@ -18,7 +18,7 @@ vector<pair<int, int> > cycle_id;
 vector<bool> is_root;
 vector<pair<int, int> > dist_to_root;
 vector<vector<int> > dist_from_root, cycles;
-
+ 
 /// @brief Update the distance to the root of u coming from v
 /// O(1)
 void update_distance_root(int u, int v) {
@@ -28,49 +28,52 @@ void update_distance_root(int u, int v) {
     
     // Update the distance
     dist_to_root[u].second = dist_to_root[v].second + 1;
-
+ 
     // Set into the matrix 
     dist_from_root[r].push_back(u);
 }
-
+ 
 /// @brief Process the graph updating distances and building cycles
 /// @param v 
 void dfs(int v) {
     state[v] = VISITING;
     int u = adj[v];
-
+ 
     if (state[u] == NOT_VISITED) {
         update_distance_root(u, v);
         dfs(u);
     } else if (state[u] == VISITING) {
         int start = u;
         int end = v;
-
+ 
         // Building the cycle
-        vector<int> path;
+        int i = 0;
+        int size = dist_to_root[end].second - dist_to_root[start].second + 1; 
+        vector<int> path(size);
         while(start != end) {
             cycle_id[start].first  = cycle_tag;
-            cycle_id[start].second = path.size();
-            path.push_back(start);
+            cycle_id[start].second = i;
+            path[i] = start;
             start = adj[start];
+            i++;
         }
         cycle_id[end].first = cycle_tag;
-        cycle_id[end].second = path.size();
-        path.push_back(end);
-
+        cycle_id[end].second = i;
+        path[i] = end;
+ 
         // Saving the length of the cycle
         cycles.push_back(path);
-
+ 
         // Incrementing cycle tag
         cycle_tag++;
     } else {
         update_distance_root(u, v);
         if (cycle_id[u].first == -1) dfs(u);
     }
-
+ 
     state[v] = VISITED;
 }
-
+ 
 void cycles_and_distances() {
     for(int v = 1; v <= n; v++)
         if (state[v] == NOT_VISITED) {
@@ -79,7 +82,7 @@ void cycles_and_distances() {
             dfs(v);
         }
 }
-
+ 
 // O(1)
 int process_cycle(int v, int k) {
     int position = cycle_id[v].second;
@@ -87,60 +90,60 @@ int process_cycle(int v, int k) {
     int size     = cycles[id].size();
     return cycles[id][(k + position) % size];
 }
-
+ 
 // O(1)
 int process_query(int v, int k) {
     if (cycle_id[v].first != -1) return process_cycle(v, k);
-
+ 
     // Values assuming v is a root
     int teleports = k;
     int root      = v;
-
+ 
     if (!is_root[v]) {
         root      = dist_to_root[v].first;
         teleports = k + dist_to_root[v].second;
     } 
-
+ 
     int size = dist_from_root[root].size();
-
+ 
     if (teleports < size) return dist_from_root[root][teleports];
-
+ 
     int from = dist_from_root[root][size - 1];
     teleports -= (size - 1);
-
+ 
     if (cycle_id[from].first == -1) from = adj[from];
     
     return process_cycle(from, teleports);
 }
-
+ 
 void destinations() {
     for(query &q: queries)
         cout << process_query(q.first, q.second) << endl;
 }
-
+ 
 int main() {
     cin >> n >> q;
-
+ 
     adj.assign(n+1, 0);
     state.assign(n+1, NOT_VISITED);
     is_root.assign(n+1, true);
     cycle_id.assign(n+1, make_pair(-1,-1));
     dist_to_root.assign(n+1, make_pair(-1,-1));
     dist_from_root.assign(n+1, vector<int>(1, -1));
-
+ 
     for(int i = 1; i <= n; i++) {
         int v; cin >> v;
         is_root[v] = false;
         adj[i] = v;
     }
-
+ 
     while(q--) {
         int x, k; cin >> x >> k;
         queries.push_back(make_pair(x,k));
     }
-
+ 
     cycles_and_distances(); // O(n)
-
+ 
     for(int r = n+1; r <= n; r++) {
         if (is_root[r]) {
             cout << r << ": ";
@@ -150,6 +153,6 @@ int main() {
             cout << endl;
         }
     }
-
+ 
     destinations(); // O(n)
 }
