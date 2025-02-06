1def174dbc1c0042b57b7ac4f4ff85c4baca9128
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-02-06 14:34:45 -0300

    Template

---
+++ defs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int> > mat;
+typedef pair<int,int> pi;
+
+#define pb push_back
+#define mp make_pair
+
+#define forn(i,a,b) for (int i = a; i < b; i++)
+
+const int INF  = 1e9;
+const int MAXN = 0;
+const int mod  = 1000000007;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+}
