99fa2b069ef07582b421760d3efcd0666fcf485c
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-04-23 15:44:11 -0300

    Agrega comentarios

--- solutions/projects.cpp
+++ solutions/projects.cpp
@@ -28,6 +28,7 @@ int main() {
         projects.emplace_back(a, b, p);
     }
 
+    // Ordeno segun el día en el que empiezan
     sort(projects.begin(), projects.end(), [](Project const &lhs, Project const &rhs){
         return lhs.starting_day < rhs.starting_day;
     });
@@ -35,14 +36,18 @@ int main() {
     forn(i, 0, n) 
         start_times[i] = projects[i].starting_day;
     
-
+    // Memo que representa la máxima cantidad de dinero ganado hasta el i-esimo
     vector<ll> memo(n + 1, 0);  
     
+    // Enfoque bottom up
     forn(i, 0, n) {
+        // Comparamos el máximo dinero ganado hasta ahora con el valor actual contando el próximo proyecto
         memo[i + 1] = max(memo[i + 1], memo[i]);
         
+        // Buscamos el próximo proyecto en log n
         int next_project = lower_bound(start_times.begin(), start_times.end(), projects[i].ending_day+1) - start_times.begin();
 
+        // Comparamos el máximo dinero ganado hasta ahora + elegir este proyecto contra el valor actual contando el próximo proyecto
         memo[next_project] = max(memo[next_project], memo[i] + projects[i].reward);
     }
 
