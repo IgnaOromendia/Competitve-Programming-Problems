fc4b06b51527526fe8abe31ba38b88e74645fd6d
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-04-28 20:09:56 -0300

    Elevator rides accepted

--- solutions/elevator_rides.cpp
+++ solutions/elevator_rides.cpp
@@ -1,62 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <bitset>
+#include <climits>
 
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<vector<int> > mat;
 typedef pair<int,int> pi;
+typedef vector<pi> vpi;
+typedef vector<vector<int> > mat;
+typedef pair<double,double> pd;
+typedef vector<mat> tensor;
 
 #define pb push_back
 #define mp make_pair
 
-#define forn(i,a,b) for (int i = a; i < b; i++)
+#define forn(i,from,to) for (int i = from; i < to; i++)
 
-const int INF  = 1e9;
-const int MAXN = 0;
-const int mod  = 1000000007;
-
+constexpr int INF  = 1e9;
+constexpr int MAXN = 20;
+constexpr int mod  = 1e9 + 7;
+constexpr double PI = 3.14159265359;
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int n, max_weight; cin >> n >> max_weight;
+    int n, x; cin >> n >> x;
 
-    // <min amount of rides, min weight of the last ride>
-    pi memo[1 << n];
-    int weights[n];
+    vpi memo(1 << n);
+    vi weights(n);
+    int people = (1 << n) - 1;
 
-    forn(i, 0, n) cin >> weights[i];
-   
-    memo[0] = {1, 0};
+    forn(i, 0, n) cin >> weights[i];    
 
-    forn(s, 1, 1 << n) {
-        memo[s] = {n+1, 0};
+    // Memorizamos para cada subconjunto que todavía no subió la cantidad de viajes y la suma de pesos
+    memo[0] = mp(1, 0);
 
-        forn(p, 0, n) {
-            // Item si included in the set
-            if (s & (1 << p)) {
-                // Subset without p
-                pi option = memo[s^(1 << p)];
+    // Iteramos sobre todos los subconjuntos posibles
+    forn(S, 1, 1 << n) {
+        memo[S] = mp(INF, 0);
+        
+        // Por cada persona i me fijo que no haya subido y pruebo hacerlo subir
+        forn(i, 0, n) if (S & (1 << i)) {
+            // Elimino a i de los que no viajaron todavía
+            int S2 = S & ~(1 << i);
 
-                if (option.second + weights[p] <= max_weight){ 
-                    option.second += weights[p];
-                } else {
-                    option.first++;
-                    option.second = weights[p];
-                }
+            auto [rides, w] = memo[S2];
 
-                memo[s] = min(memo[s], option);
-            }
-            
+            // En caso de que nos excedamos aumentamos la cantidad de viajes y reincicamos el peso
+            rides += w + weights[i] <= x ? 0 : 1;
+            int curr_weigth = weights[i] + ( w + weights[i] <= x ? w : 0);
+
+            memo[S] = min(memo[S], mp(rides, curr_weigth));
         }
     }
 
-    cout << memo[(1 << n)-1].first << endl;
-
-    return 0;
+    cout << memo[people].first << endl;
 
 }
