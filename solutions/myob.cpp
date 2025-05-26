37dc8d5d14d9e07af34db4b8987b71e67189823f
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-05-26 17:34:48 -0300

    Mining your own business accepted

--- solutions/myob.cpp
+++ solutions/myob.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <bitset>
 #include <algorithm>
+#include <tuple>
+#include <set>
 
 using namespace std;
 
@@ -17,13 +19,13 @@ typedef pair<double,double> pd;
 #define forn(i,from,to) for (int i = from; i < to; i++)
 
 constexpr int INF  = 1e9;
-constexpr int MAXN = 5 * 1e4;
+constexpr int MAXN = 5 * 1e4 + 1;
 constexpr int mod  = 1e9 + 7;
 constexpr double PI = 3.14159265359;
 
 int n; 
 mat adj; 
-vi tin, low, visited, children, iscutpoint;
+vi tin, low, visited, color, isCutPoint;
 int timer;
 
 // Contar solo los q estan adyacentes a exactamente uno de los pts de art
@@ -31,6 +33,7 @@ int timer;
 void dfs_cutpoints(int v, int p = -1) {
     visited[v] = true;
     tin[v] = low[v] = timer++;
+    int children = 0;
     for (int to : adj[v]) {
         if (to == p) continue;
         if (visited[to]) {
@@ -38,11 +41,11 @@ void dfs_cutpoints(int v, int p = -1) {
         } else {
             dfs_cutpoints(to, v);
             low[v] = min(low[v], low[to]);
-            if (low[to] >= tin[v] && p!=-1) iscutpoint[v] = true;
-            children[v]++;
+            if (low[to] >= tin[v] && p!=-1) isCutPoint[v] = true;
+            children++;
         }
     }
-    if(p == -1 && children[v] > 1) iscutpoint[v] = true;
+    if(p == -1 && children > 1) isCutPoint[v] = true;
 }
 
 void find_cutpoints() {
@@ -55,54 +58,67 @@ void find_cutpoints() {
             dfs_cutpoints (i);   
 }
 
-int dfs(int v) {
+int dfs(int v, set<int>& cutPoints) {
     visited[v] = true;
     int nodes = 1;
-    for(int to: adj[v])
-        if (!visited[to] and !iscutpoint[to])
-            nodes += dfs(to);
+    for(int to: adj[v]) {
+        if (isCutPoint[to]) cutPoints.insert(to);
+        if (!visited[to] and !isCutPoint[to]) 
+            nodes += dfs(to, cutPoints);
+    }
+        
     return nodes;
 }
 
 pair<ll, int> count_ways_shaft() {
     visited.assign(n, 0);
-    ll nodes = 1;
     int shafts = 0;
+    ll ways = 1;
+    set<int> cutPoints;
 
     forn(i,0,n)
-        if (!visited[i] and !iscutpoint[i]) {
-            shafts++;
-            nodes *= dfs(i);
+        if (!visited[i] and !isCutPoint[i]) {
+            cutPoints.clear();
+            int nodes = dfs(i, cutPoints);
+            // cout << i+1 << " " << cutPoints.size() << " " << nodes << endl;
+            if (cutPoints.size() == 1) {
+                shafts++;
+                ways *= nodes;
+            }
         }
+
+    if (shafts == 0) {
+        ways   = 1LL * n * (n - 1) / 2;
+        shafts = 2;
+    }
             
-    return mp(nodes, shafts);
+    return mp(ways, shafts);
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
 
-    int t = 1, shafts;
+    int t = 1, shafts, m;
     ll ways;
 
-    while(cin >> n, n) {
+    while(cin >> m, m) {
         adj.assign(MAXN, vi());
-        children.assign(MAXN, 0);
-        iscutpoint.assign(MAXN,0);
+        isCutPoint.assign(MAXN, 0);
 
-        forn(i, 0, n) {
+        n = 0;
+
+        forn(i, 0, m) {
             int u,v; cin >> u >> v; u--;v--;
+            n = max(n, max(u,v) + 1);
             adj[u].pb(v);
             adj[v].pb(u);
         }
 
         find_cutpoints();
 
-        tie(ways, shafts) = count_ways_shaft();
-
-        forn(v, 0, n) shafts += iscutpoint[v];
+        auto [ways, shafts] = count_ways_shaft();
         
-
         cout << "Case " << t++ << ": " << shafts << " " << ways << "\n";
 
     }
