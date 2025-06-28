ec48d3f9bf528f0e5be3b55660549ee14009ea1f
Ignacio Fernandez Oromendia <ioromendia@dc.uba.ar>
2025-06-28 15:46:06 -0300

    Optimiza implementación

--- templates/scc.cpp
+++ templates/scc.cpp
@@ -7,39 +7,35 @@ typedef vector<int> vi;
 typedef vector<vector<int> > graph;
 #define forn(i,from,to) for (int i = from; i < to; i++)
 
-
-#define NOT_VISITED -1
-#define PROCESSING 0
-#define VISITED 1
-
 graph adj, adjT;
-vi order, state, strong_component;
-int n;
+vi order, visited, strong_component;
+int n, visitTimer = 1;
 
-void dfs(int v, vector<vector<int> > &adj) {
+void dfs(int v, graph& adj) {
+    visited[v] = visitTimer;
+
     for(int u: adj[v])
-        if(state[u] == NOT_VISITED) {
+        if(visited[u] != visitTimer) {
             strong_component[u] = strong_component[v];
             dfs(u, adj);
         }
     
-    state[v] = VISITED;
     order.push_back(v);
 }
 
 void find_strong_components() {
     forn(i,0,n)
-        if(state[i] == NOT_VISITED)
+        if(visited[i] != visitTimer)
             dfs(i, adj);
 
 
-    state            = vector<int>(n+1, NOT_VISITED);
+    visitTimer++;
     strong_component = vector<int>(n+1, 0);
     int count = 0;
 
-    while(!order.empty()) {
-        int v = order.back(); order.pop_back();
-        if(state[v] == NOT_VISITED) {
+    for(int i = n-1; i >= 0; i--) {
+        int v = order[i];
+        if(visited[v] != visitTimer) {
             count++;
             strong_component[v] = count;
             dfs(v, adjT);
